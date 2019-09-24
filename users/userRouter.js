19fc24145fec1ad16c2befc410776b0f1eac5e10
Megan White <mfw247@gmail.com>
2019-09-24 14:19:29 -0700

    created endpoint to get all users

--- users/userRouter.js
+++ users/userRouter.js
@@ -43,4 +43,15 @@ router.post("/login",async(req,res) => {
     }
 })
 
+// view list of users
+router.get("/", async(req,res) => {
+    try {
+        const users = await Users.getUsers()
+        res.status(200).json(users)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:'Could not retrieve users'})
+    }
+})
+
 module.exports = router
